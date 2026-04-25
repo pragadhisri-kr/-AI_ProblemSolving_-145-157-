3adaae572b9570e0fe12ba1eebdb1af3520f1d69
pragadhisri-kr <pragadhisri@gmail.com>
2026-04-25 21:07:12 +0530

    Implement BFS to find steps to goal state

--- warehouse_robot/main.py
+++ warehouse_robot/main.py
@@ -1,1 +1,11 @@
+from state import initial_state, goal_state
+from planner import bfs
 
+result = bfs(initial_state, goal_state)
+
+if result:
+    print("Steps to reach goal:")
+    for step in result:
+        print(step)
+else:
+    print("No solution found")
